5e298bdd9e837cad51ced1be0666561307ee226e
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2022-05-08 21:36:28 +0530

    Day3completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/home_page.dart';
+import 'package:test_app/pages/home_page.dart';
+import 'package:test_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
